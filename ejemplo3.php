244f31006192d79fbecb6a713ecff1d64b55dbf0
juanAvila <alumno@isbo.edu.uy>
2024-04-03 09:39:48 -0300

    ejemplo3 agregado, variables agregadas, incompleto

---
+++ ejemplo3.php
@@ -0,0 +1,15 @@
+<html>
+    <head></head>
+    <body>
+        <h1>mi segundo archivo php</h1>
+        <?php
+        $nombre ='jose';
+        $edad =31;
+        $altura =1.66;
+        $mayorEdad = true;
+        echo 'Nombre:'$nombre;
+        echo 'edad:'
+        echo
+        ?>
+    </body>
+</html>
