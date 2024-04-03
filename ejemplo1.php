648fd5fadaf94482002bc8a9c6cf409c8a2a93a0
juanAvila <alumno@isbo.edu.uy>
2024-04-03 08:48:05 -0300

    comentarios agregados

--- ejemplo1.php
+++ ejemplo1.php
@@ -4,7 +4,15 @@
         <h1>letra</h1>
         <?php
             echo 'no se si hice bien, pero llamé. ';
-        echo 'no me gusto lo que escuche';
+            /*Las mitocondrias son orgánulos celulares eucariotas
+            encargados de suministrar la mayor parte de la energía
+            necesaria para la actividad celular a través del proceso
+            denominado respiración celular.
+            Actúan como centrales energéticas de la célula y sintetizan
+            ATP a partir de los combustibles metabólicos.*/ 
+            echo 'no me gusto lo que escuche';
+            
         ?>
+
     <body>
 </html>
--- ejemplo2.php
+++ ejemplo2.php
@@ -3,10 +3,12 @@
     <body>
         <h1>mi segundo archivo php</h1>
         <?php
+        //benbiemido a la recistencia contra las maquinas
         echo <<<END
         en un mundo conectado,
         donde en cada segundo se envian millones de correos electronicos...
         END;
+        #si nos comunicamos de esta forma no nos pueden detectar
         ?>
     </body>
 </html>
