b1fa54c10c2ba0e867fd221ca7cf91453e0948e9
juanAvila <alumno@isbo.edu.uy>
2024-04-03 08:13:13 -0300

    creacion del archivo agregado el h1 y el php

---
+++ ejemplo1.php
@@ -0,0 +1,10 @@
+<html>
+    <head></head>
+    <body>
+        <h1>letra</h1>
+        <?php
+            echo 'no se si hice bien, pero llamé. ';
+        echo 'no me gusto lo que escuche';
+        ?>
+    <body>
+</html>
