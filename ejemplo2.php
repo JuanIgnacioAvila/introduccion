34f11c5411e269cd20c8a033c022483562148fa7
juanAvila <alumno@isbo.edu.uy>
2024-04-03 08:35:19 -0300

    creacion del ejemplo2

---
+++ ejemplo2.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+        <h1>mi segundo archivo php</h1>
+        <?php
+        echo <<<END
+        en un mundo conectado,
+        donde en cada segundo se envian millones de correos electronicos...
+        END;
+        ?>
+    </body>
+</html>
